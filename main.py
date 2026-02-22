1ddb8d85b954956a3096a0f70b1418b2d8f44a1c
Maksym Severyn <maksymko.letter@gmail.com>
2026-02-22 21:11:48 +0200

    Created signin html and added logic for it in main

--- main.py
+++ main.py
@@ -7,25 +7,50 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/")
-async def read_index():
-    return FileResponse('templates/signup.html')
+async def signin_page():
+    return FileResponse("templates/signin.html")
+
+
+@app.get("/signup")
+async def signup_page():
+    return FileResponse("templates/signup.html")
+
 
 @app.post("/signup")
-async def handle_registration(username_placeholder: str = Form(...),
-                                email_placeholder: str= Form(...),
-                                password_placeholder: str = Form(...),
-                                confirm_pasword: str = Form(...)):
+async def handle_registration(
+    username_placeholder: str = Form(...),
+    email_placeholder: str = Form(...),
+    password_placeholder: str = Form(...),
+    confirm_pasword: str = Form(...),
+):
 
     print(f"New User: {username_placeholder}")
     print(f"New User's email: {email_placeholder}")
     print(f"New User's password: {password_placeholder}")
-    
+
     return {
         "status": "Success",
         # "message": f"Welcome, {username_placeholder}! You are now registered." NOT NEEDED FOR NOW
     }
 
 
+@app.post("/signin")
+async def handle_signing_in(
+    username_email: str = Form(...),
+    password: str = Form(...),
+):
+    # Тепер дані з форми потраплять сюди
+    print(f"Login attempt: {username_email}")
+    print(f"Password used: {password}")
+
+    return {
+        "status": "Success",
+        "user": username_email,
+        "message": "Logged in successfully",
+    }
+
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="127.0.0.1", port=8000)
