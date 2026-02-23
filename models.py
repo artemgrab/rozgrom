dcfdefe068b217f1089b34b011b205ff6ed61c85
Oleksandr Melnychenko <oleksandr.melnychenko.main@gmail.com>
2026-02-23 22:27:24 +0200

    Moving database and auth logic to its own branch

---
+++ models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, DateTime
+from sqlalchemy.sql import func
+from database import Base
+from sqlalchemy.orm import relationship
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    join_time = Column(DateTime, server_default=func.now())        # When user signed up
+    
+    messages_sent = relationship("Message", backref="sender", foreign_keys="Message.sender_id")
+
+
+class Message(Base):
+    __tablename__ = "messages"
+
+    id = Column(Integer, primary_key=True, unique=True, index=True)
+    content = Column(Text)
+    timestamp = Column(DateTime, server_default=func.now())
+
+    sender_id = Column(Integer, ForeignKey("users.id"))
+    reciever_id = Column(Integer, ForeignKey("users.id"))
