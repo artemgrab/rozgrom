38851146048e8b41e57c848da3b44122c45dd48d
Oleksandr Melnychenko <oleksandr.melnychenko.main@gmail.com>
2026-02-24 15:06:24 +0200

    Refactored newadded code

--- routers/auth.py
+++ routers/auth.py
@@ -1,31 +1,39 @@
-from fastapi import APIRouter, Form, status
+from fastapi import APIRouter, Form, status, Request
 from fastapi.responses import FileResponse, RedirectResponse
-
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
 
+# Tells FastAPI where html files are
+templates = Jinja2Templates(directory="templates")
 
+
+# Opens "welcome" page
 @router.get("/")
-async def welcome_page():
-    return FileResponse("templates/welcome.html")
+async def welcome_page(request: Request):
+    return templates.TemplateResponse("welcomeToDuplo.html", {"request": request})
 
 
+# Opens "sign-in" page
 @router.get("/signin")
-async def signin_page():
-    return FileResponse("templates/sign-in.html")
+async def signin_page(request: Request):
+    return templates.TemplateResponse("sign-in.html", {"request": request})
 
 @router.post("/signin")
 async def handle_signing_in(
-    email_placeholder: str = Form(...),
-    password_placeholder: str = Form(...),
+    email: str = Form(...),
+    password: str = Form(...),
 ):
-
+    
+    print("Works correctly")
+    
     return RedirectResponse(url="/chats", status_code=status.HTTP_303_SEE_OTHER)
 
 
+# Opens "sign-up" page
 @router.get("/signup")
-async def signup_page():
-    return FileResponse("templates/signup.html")
+async def signup_page(request: Request):
+    return templates.TemplateResponse("sign-up.html", {"request": request})
 
 @router.post("/signup")
 async def handle_registration(
@@ -35,8 +43,10 @@ async def handle_registration(
     confirm_password: str = Form(...),
 ):
 
+    print("Works correctly")
+
     #! One of future implementations
     # if len(password) > 71: return {"error": "Password shouldn't be longer than 71 cahracters"}
     # elif len(password) < 8: return {"error": "Password should be at least 8 long"}
     
-    return RedirectResponse(url="/chats", status_code=status.HTTP_303_SEE_OTHER)
+    return RedirectResponse(url="/signin", status_code=status.HTTP_303_SEE_OTHER)
